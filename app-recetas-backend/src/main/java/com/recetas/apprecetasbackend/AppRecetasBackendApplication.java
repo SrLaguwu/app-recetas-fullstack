dc8121ab8e22d22e58eef7db14ccfd3c7b492ee5
Srlaguwu <eldiabloeslomejor@hotmail.com>
2025-06-12 10:37:31 -0500

    Commit inicial: Monorepo para toda la app (Angular frontend + SpringBoot Backend)

---
+++ app-recetas-backend/src/main/java/com/recetas/apprecetasbackend/AppRecetasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recetas.apprecetasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRecetasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRecetasBackendApplication.class, args);
+	}
+
+}
---
+++ app-recetas-backend/src/test/java/com/recetas/apprecetasbackend/AppRecetasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recetas.apprecetasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRecetasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
